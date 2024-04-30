90c406b01fc4419393c8e1e568f3526b4c48ebb5
alkurlaev <alkurlaev@yandex.ru>
2024-04-30 18:42:52 +0300

    Changed game colors

--- core/src/main/java/com/alkurlaev/reversi/Constants.java
+++ core/src/main/java/com/alkurlaev/reversi/Constants.java
@@ -12,11 +12,11 @@ public class Constants {
 
     public static final float GAME_CELL_SIZE = (float) GAME_RESOLUTION / GAME_FIELD_SIZE;
 
-    public static final Color BASE_CELL_FIELD_COLOR = Color.LIGHT_GRAY;
-    public static final Color HOVERED_CELL_COLOR = new Color(Color.GRAY.r, Color.GRAY.g, Color.GRAY.b, 0.6f);
-    public static final Color FIELD_SEPARATION_LINE_COLOR = Color.DARK_GRAY;
-    public static final Color BLACK_CELL_COLOR = Color.BLACK;
-    public static final Color WHITE_CELL_COLOR = Color.WHITE;
+    public static final Color BASE_CELL_FIELD_COLOR = new Color(168.0f / 255.0f, 218.0f / 255.0f, 220.0f / 255.0f, 1.0f);
+    public static final Color HOVERED_CELL_COLOR = new Color(69.0f / 255.0f, 123.0f / 255.0f, 157.0f / 255.0f, 0.6f);
+    public static final Color FIELD_SEPARATION_LINE_COLOR = new Color(29.0f / 255.0f, 53.0f / 255.0f, 87.0f / 255.0f, 1.0f);
+    public static final Color BLACK_CELL_COLOR = new Color(230.0f / 255.0f, 57.0f / 255.0f, 70.0f / 255.0f, 1.0f);
+    public static final Color WHITE_CELL_COLOR = new Color(241.0f / 255.0f, 250.0f / 255.0f, 238.0f / 255.0f, 1.0f);
 
     public static final float HOVERED_CHIP_COLOR_ALPHA_CHANNEL = 0.5f;
 
